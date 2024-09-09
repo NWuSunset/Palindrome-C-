1625a1aae6b8c1100c02ddb9170d41b0c3b7b06f
U-BSD\476275 <476275@SHS-95W2NW3.beaverton.k12.or.us>
2024-09-09 11:49:04 -0700

    still need to work on removing punctuation from the thing

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -12,15 +12,12 @@ int removeElement(char arr[], int len, char rem) {
     if (arr[i] == rem) { //if the element to remove is found
       len = len - 1; //make the length of the array shorter
         for (int j = i; j < len; j++) { //move all the elements right till the end of the new length
-	  //cout << "before: " << arr[j] << endl;
 	  arr[j] = arr[j + 1];
-	  //cout << "after: " << arr[j] << endl;
         }
     }
   }
   return len;
 }
-
 
 int main() {
   //this gonna palindrome time
@@ -30,26 +27,34 @@ int main() {
   cin.get();
   //send error if more than 80
 
-  int strLength = strlen(str) - 1; //subtract one for the null character
+  int strLength = strlen(str);
   int arrLen = sizeof(str) / sizeof(str[0]); 
+  
+  int remCount = 0; //Needed for reversing as whenever an element is removed it adds an extra blank characters
+  //Remove all non-letters
 
-  //Remove all non-letters 
-  for (int i = 0; i < arrLen - 1; i++) {
+  int remChar = 0;
+  for (int i = 0; i < strlen(str); i++) {
     if (ispunct(str[i]) || isspace(str[i])) {
-      arrLen = removeElement(str, arrLen, str[i]); 
+      remChar = i;
+      remCount++;
      }
   }
-
-  /* int strLength = strlen(str) - 1; //subtract one for the null character  
+  if (remCount > 0) {
+    arrLen = removeElement(str, arrLen, str[remChar]);
+  }
+  cout << str << endl;
+  cout << remCount << endl;
+  int n = strLength - 1;
   //Then reverse the array
-  for (int i = 0; i < strlen(str); i++) {
-    str2[i] = str[strLength - i]; //go backwards through str and put that into str2 
-    } */
+  for (int i = 0; i < strLength; i++) { //go through length of string
+    cout << str[n - remCount] << endl;
+    str2[i] = str[n - remCount]; //assign the values of the string in backwards.
+    n--;
+    }
 
   cout << str << endl;
-  for (int i = 0; i < arrLen; i++)
-    cout << str[i] << " "; //printing out some weird stuff right now. (probably the null char or something)
-  
+  cout << str2 << endl;
   return 0;
 }
 
