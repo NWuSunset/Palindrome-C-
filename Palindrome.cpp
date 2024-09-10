017a66afcbf5b39208ac8567bd502a1b0f9eebd3
U-BSD\476275 <476275@SHS-95W2NW3.beaverton.k12.or.us>
2024-09-10 09:12:28 -0700

    still not working :(

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -8,16 +8,20 @@ using namespace std;
 //Functions have to be declared before main
 //remove an element (rem) from a char array
 int removeElement(char arr[], int len, char rem) {
-  for (int i = 0; i < len; i++) {
-    if (arr[i] == rem) { //if the element to remove is found
+  int i;
+  for (i = 0; i < len; i++) {
+    if (arr[i] == rem)  //if the element to remove is found
+      break;
+
+    if (i < len) {
       len = len - 1; //make the length of the array shorter
         for (int j = i; j < len; j++) { //move all the elements right till the end of the new length
 	  arr[j] = arr[j + 1];
         }
-    }
+  }
   }
   return len;
-}
+} arrLen = removeElement(str, arrLen, str[remChar]);
 
 int main() {
   //this gonna palindrome time
@@ -36,20 +40,21 @@ int main() {
   int remChar = 0;
   for (int i = 0; i < strlen(str); i++) {
     if (ispunct(str[i]) || isspace(str[i])) {
-      remChar = i;
-      remCount++;
+       arrLen = removeElement(str, arrLen, str[i]);
+       //remChar = i;
+       //remCount++;
      }
   }
   if (remCount > 0) {
-    arrLen = removeElement(str, arrLen, str[remChar]);
+    //    arrLen = removeElement(str, arrLen, str[remChar]);
   }
   cout << str << endl;
   cout << remCount << endl;
   int n = strLength - 1;
   //Then reverse the array
   for (int i = 0; i < strLength; i++) { //go through length of string
-    cout << str[n - remCount] << endl;
-    str2[i] = str[n - remCount]; //assign the values of the string in backwards.
+    cout << str[n] << endl;
+    str2[i] = str[n]; //assign the values of the string in backwards.
     n--;
     }
 
